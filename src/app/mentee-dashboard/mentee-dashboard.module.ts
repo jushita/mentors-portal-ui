a6be369727e4a4355c0f16093e07e5cf72802422
jushita <jushitaa@gmail.com>
2020-06-27 13:38:58 -0500

    grid created

--- src/app/mentee-dashboard/mentee-dashboard.module.ts
+++ src/app/mentee-dashboard/mentee-dashboard.module.ts
@@ -1,13 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MenteeDashboardComponent } from './mentee-dashboard/mentee-dashboard.component';
+import { SharedModule } from '../shared/shared.module';
+import { MenuModule } from '../menu/menu.module';
 
 
 
 @NgModule({
   declarations: [MenteeDashboardComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    SharedModule,
+    MenuModule,
+    MenuModule
+  ],
+  exports: [
+    MenteeDashboardComponent
   ]
 })
 export class MenteeDashboardModule { }
